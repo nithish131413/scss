6f5585d5b0ca70eb5c820eda5f7dac65b3ae91c4
nithish131413 <120095968+nithish131413@users.noreply.github.com>
2023-01-03 06:32:26 -0500

    scss file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+    <p className='nithish'>nithish is learning</p>
+    <h1 className='name'>HI this</h1>
+    <div className='btn'>
+      <button className='btn-btn'>button1</button> <br/>
+      <button className='btn1'>button2</button>
+      <p className='elm'>This element has a margin of 70px.</p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
